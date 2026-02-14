b63562c46459c0d3d1eae28bf3cba1e160adf504
koribot <w.cawasa@gmail.com>
2026-02-14 20:38:42 +0800

    icon stuffs

--- build-dev.js
+++ build-dev.js
@@ -1,16 +1,27 @@
 const { execSync } = require("child_process");
 const os = require("os");
 
-let gccCommand = "gcc ./src/main.c -o kort";
+console.log("Running development build...");
+
+let gccCommand = "";
 
 if (os.platform() === "win32") {
-  gccCommand += ".exe -g -O0 -Wall -I./src/include -L./src/lib -lraylib -lgdi32 -lwinmm";
+  // Step 1: Compile the icon resource
+  console.log("Compiling Windows icon resource...");
+  execSync("windres ./icon.rc ./icon.res --output-format=coff", { stdio: "inherit" });
+
+  // Step 2: Build the .exe including the resource
+  gccCommand =
+    "gcc ./src/main.c ./icon.res -g -O0 -Wall -I./src/include -L./src/lib -lraylib -lgdi32 -lwinmm -o kort.exe";
 } else if (os.platform() === "linux") {
-  gccCommand += " -g -O0 -Wall -I./src/include -L./src/lib -lraylib -lGL -lm -ldl -lpthread";
+  gccCommand =
+    "gcc ./src/main.c -g -O0 -Wall -I./src/include -L./src/lib -lraylib -lGL -lm -ldl -lpthread -o kort";
 } else if (os.platform() === "darwin") {
-  gccCommand += " -g -O0 -Wall -I./src/include -L./src/lib -lraylib -framework OpenGL -framework Cocoa -framework IOKit -framework CoreAudio";
+  gccCommand =
+    "gcc ./src/main.c -g -O0 -Wall -I./src/include -L./src/lib -lraylib -framework OpenGL -framework Cocoa -framework IOKit -framework CoreAudio -o kort";
 }
 
-console.log("Running development build:");
 console.log(gccCommand);
 execSync(gccCommand, { stdio: "inherit" });
+
+console.log("Build finished successfully!");
